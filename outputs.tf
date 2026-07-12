6684202214c7743da8db0c90ca9d61493fbef89d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "stack_hci_storage_paths" {
-  description = "All stack_hci_storage_path resources"
-  value       = azurerm_stack_hci_storage_path.stack_hci_storage_paths
-}
 output "stack_hci_storage_paths_custom_location_id" {
-  description = "List of custom_location_id values across all stack_hci_storage_paths"
-  value       = [for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : v.custom_location_id]
+  description = "Map of custom_location_id values across all stack_hci_storage_paths, keyed the same as var.stack_hci_storage_paths"
+  value       = { for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : k => v.custom_location_id }
 }
 output "stack_hci_storage_paths_location" {
-  description = "List of location values across all stack_hci_storage_paths"
-  value       = [for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : v.location]
+  description = "Map of location values across all stack_hci_storage_paths, keyed the same as var.stack_hci_storage_paths"
+  value       = { for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : k => v.location }
 }
 output "stack_hci_storage_paths_name" {
-  description = "List of name values across all stack_hci_storage_paths"
-  value       = [for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : v.name]
+  description = "Map of name values across all stack_hci_storage_paths, keyed the same as var.stack_hci_storage_paths"
+  value       = { for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : k => v.name }
 }
 output "stack_hci_storage_paths_path" {
-  description = "List of path values across all stack_hci_storage_paths"
-  value       = [for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : v.path]
+  description = "Map of path values across all stack_hci_storage_paths, keyed the same as var.stack_hci_storage_paths"
+  value       = { for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : k => v.path }
 }
 output "stack_hci_storage_paths_resource_group_name" {
-  description = "List of resource_group_name values across all stack_hci_storage_paths"
-  value       = [for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : v.resource_group_name]
+  description = "Map of resource_group_name values across all stack_hci_storage_paths, keyed the same as var.stack_hci_storage_paths"
+  value       = { for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : k => v.resource_group_name }
 }
 output "stack_hci_storage_paths_tags" {
-  description = "List of tags values across all stack_hci_storage_paths"
-  value       = [for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : v.tags]
+  description = "Map of tags values across all stack_hci_storage_paths, keyed the same as var.stack_hci_storage_paths"
+  value       = { for k, v in azurerm_stack_hci_storage_path.stack_hci_storage_paths : k => v.tags }
 }
 
